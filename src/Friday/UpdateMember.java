42fe34b7d42faa8fa19283e13509eaac15c04959
maxoseman14 <maxoseman@gmail.com>
2018-07-14 16:37:59 +0100

    14/07/18

---
+++ src/Friday/UpdateMember.java
@@ -0,0 +1,87 @@
+package Friday;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class UpdateMember extends JFrame {
+
+    JPanel p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16,
+    p17, p18, p19, p20, p21, p22, p23;
+
+
+    public UpdateMember() throws HeadlessException {
+
+        setVisible(true);
+        setSize(400, 800);
+        setLayout(new GridLayout(22,1));
+
+        p1 = new JPanel (new GridLayout(1, 3));
+        p2 = new JPanel();
+        p3 = new JPanel (new GridLayout(1,5));
+        p4 = new JPanel();
+        p5 = new JPanel (new GridLayout(1,5));
+        p6 = new JPanel();
+        p7 = new JPanel (new GridLayout(1,5));
+        p8 = new JPanel();
+        p9 = new JPanel (new GridLayout(1,5));
+        p10 = new JPanel();
+        p11 = new JPanel (new GridLayout(1,5));
+        p12 = new JPanel();
+        p13 = new JPanel (new GridLayout(1,5));
+        p14 = new JPanel();
+        p15 = new JPanel (new GridLayout(1,5));
+        p16 = new JPanel();
+        p17 = new JPanel (new GridLayout(1,5));
+        p18 = new JPanel();
+        p19 = new JPanel (new GridLayout(1,5));
+        p20 = new JPanel();
+        p21 = new JPanel (new GridLayout(1,5));
+        p22 = new JPanel();
+        p23 = new JPanel (new GridLayout(1, 7));
+
+        Update_QA = new JLabel("Update QA Member");
+        l1 = new JLabel(" ");
+        l2 = new JLabel(" ");
+        l3 = new JLabel(" ");
+        l4 = new JLabel(" ");
+        l5 = new JLabel(" ");
+        l6 = new JLabel(" ");
+        l7 = new JLabel(" ");
+        l8 = new JLabel(" ");
+        l9 = new JLabel(" ");
+        l10 = new JLabel(" ");
+        l11 = new JLabel(" ");
+        l12 = new JLabel(" ");
+        l13 = new JLabel(" ");
+        l14 = new JLabel(" ");
+        l15 = new JLabel(" ");
+        l16 = new JLabel(" ");
+        l17 = new JLabel(" ");
+        l18 = new JLabel(" ");
+        l19 = new JLabel(" ");
+        l20 = new JLabel(" ");
+        l21 = new JLabel(" ");
+        l22 = new JLabel(" ");
+        l23 = new JLabel(" ");
+        QA_ID = new JLabel(" ");
+        first_name = new JLabel(" ");
+        last_name = new JLabel(" ");
+        email = new JLabel(" ");
+        role_ID = new JLabel(" ");
+        client_ID = new JLabel(" ");
+        start = new JLabel(" ");
+        end = new JLabel(" ");
+        gender = new JLabel(" ");
+
+
+        
+    }
+
+
+
+
+
+
+
+
+}
